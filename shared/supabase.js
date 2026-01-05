b186e8c45fe8df4c0e4145c289239634f6b0784b
prachtadam <pracht_adam@hotmail.com>
2026-01-04 19:54:01 -0600

    Update supabase.js

--- shared/supabase.js
+++ shared/supabase.js
@@ -1,1 +1,10 @@
 
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const SUPABASE_URL = "PASTE_PROJECT_URL_HERE";
+export const SUPABASE_ANON_KEY = "PASTE_ANON_PUBLIC_KEY_HERE";
+
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
