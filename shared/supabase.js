9531d541d87d780eb863321ad975e6e3a559a5cf
prachtadam <pracht_adam@hotmail.com>
2026-01-06 21:34:08 -0600

    Update supabase.js

--- shared/supabase.js
+++ shared/supabase.js
@@ -1,10 +1,8 @@
+// shared/supabase.js (GLOBAL, NOT A MODULE)
 
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+window.ORG_ID = "pracht";
 
-export const SUPABASE_URL = "https://inwrnzkzseuymfzrpocn.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_81elBWnKMo2ouw53vYQVUQ_Iyo3HHg1";
-
-export const supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+window.sb = supabase.createClient(
+  "https://inwrnzkzseuymfzrpocn.supabase.co",
+  "sb_publishable_81elBWnKMo2ouw53vYQVUQ_Iyo3HHg1"
 );
