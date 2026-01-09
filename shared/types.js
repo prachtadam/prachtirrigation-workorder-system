27c9bea3c1bfeb98d09d860af450acbaee54ec98
prachtadam <pracht_adam@hotmail.com>
2026-01-09 14:08:54 -0600

    Create types.js

---
+++ shared/types.js
@@ -0,0 +1,20 @@
+
+export const JOB_STATUSES = {
+  OPEN: 'open',
+  ON_THE_WAY: 'on_the_way',
+  ON_SITE_DIAGNOSTICS: 'on_site_diagnostics',
+  ON_SITE_REPAIR: 'on_site_repair',
+  PAUSED: 'paused',
+  FINISHED: 'finished',
+  INVOICED: 'invoiced',
+  CANCELED: 'canceled',
+};
+
+export const JOB_EVENT_TYPES = {
+  OPEN: 'open',
+  ON_THE_WAY: 'on_the_way',
+  ON_SITE_DIAGNOSTICS: 'on_site_diagnostics',
+  ON_SITE_REPAIR: 'on_site_repair',
+  PAUSED: 'paused',
+  FINISHED: 'finished',
+};
