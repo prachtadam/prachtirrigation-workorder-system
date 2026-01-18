f5be04608451123eb8d6ca0548fdb2be77b94f09
prachtadam <pracht_adam@hotmail.com>
2026-01-18 14:35:41 -0600

    Removed config.js

--- runtime-config.js
+++
@@ -1,9 +0,0 @@
-// runtime-config.js
-// Loaded before any app code
-
-window.SUPABASE_URL = "https://YOURPROJECTID.supabase.co";
-window.SUPABASE_ANON_KEY = "YOUR_PUBLIC_ANON_KEY";
-window.SUPABASE_ORG_ID = "YOUR_ORG_ID";
-
-// optional, safe to leave blank if unused
-// window.GOOGLE_MAPS_API_KEY = "";
